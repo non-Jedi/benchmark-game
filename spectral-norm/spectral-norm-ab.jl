7ee12afb7124699e3b336f09ffeb54d3cee832b3
Adam Beckmeyer <adam_gpg@thebeckmeyers.xyz>
2019-08-30 11:35:25 -0400

    Update spectral norm implementation

--- spectral-norm/spectral-norm-ab.jl
+++ spectral-norm/spectral-norm-ab.jl
@@ -1,19 +1,16 @@
 using Printf
 
-getA(i, j) = 1.0 / ((((i+j) * (i+j+1)) / 2) + i + 1)
-getAt(i, j) = getA(j, i)
+A(i, j) = inv((((i+j)*(i+j+1)) >> 1) + i + 1)
+At(i, j) = A(j, i)
 
-function A_times_u!(dest, u, A)
+function A_times_u!(dest, u, f)
     n = length(u)
-    @inbounds Threads.@threads for i in 1:n
+    @inbounds Threads.@threads for i in 0:n-1
         sum1 = 0.0
-        j = 1
-        while j < n
-            sum1 += sum((u[j], u[j+1]) .* (A(i-1, j-1), A(i-1, j)))
-#            sum1 = muladd(u[j], A(i-1, j-1), sum1)
-            j += 2
-        end#while
-        dest[i] = sum1
+        @simd for j in 1:n
+            sum1 += u[j] * f(i, j-1)
+        end#for
+        dest[i+1] = sum1
     end#for
 end#function
 
@@ -22,11 +19,11 @@ function specnorm(n)
     v = zeros(Float64, n)
     tmp = Vector{Float64}(undef, n)
 
-    for i in 1:10
-        A_times_u!(tmp, u, getA)
-        A_times_u!(v, tmp, getAt)
-        A_times_u!(tmp, v, getA)
-        A_times_u!(u, tmp, getAt)
+    for _ in 1:10
+        A_times_u!(tmp, u, A)
+        A_times_u!(v, tmp, At)
+        A_times_u!(tmp, v, A)
+        A_times_u!(u, tmp, At)
     end#for
 
     uv = vv = 0.0
