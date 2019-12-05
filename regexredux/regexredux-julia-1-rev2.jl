1ea923c3ea6abe36bb0c4eb94c748605cfb9abfa
Adam Beckmeyer <adam_gpg@thebeckmeyers.xyz>
2019-12-05 16:19:23 -0500

    Add regexredux version making use of composable threading

---
+++ regexredux/regexredux-julia-1-rev2.jl
@@ -0,0 +1,53 @@
+# The Computer Language Benchmarks Game
+# https://salsa.debian.org/benchmarksgame-team/benchmarksgame/
+#
+# contributed by Daniel Jones
+# fixed by David Campbell
+# modified by Jarret Revels, Alex Arslan, Yichao Yu
+# made (slightly) multi-threaded by Adam Beckmeyer
+
+const variants = (
+      "agggtaaa|tttaccct",
+      "[cgt]gggtaaa|tttaccc[acg]",
+      "a[act]ggtaaa|tttacc[agt]t",
+      "ag[act]gtaaa|tttac[agt]ct",
+      "agg[act]taaa|ttta[agt]cct",
+      "aggg[acg]aaa|ttt[cgt]ccct",
+      "agggt[cgt]aa|tt[acg]accct",
+      "agggta[cgt]a|t[acg]taccct",
+      "agggtaa[cgt]|[acg]ttaccct"
+)
+
+const subs = (
+    (r"tHa[Nt]", "<4>"),
+    (r"aND|caN|Ha[DS]|WaS", "<3>"),
+    (r"a[NSt]|BY", "<2>"),
+    (r"<[^>]*>", "|"),
+    (r"\|[^|][^|]*\|", "-")
+)
+
+function perf_regex_dna(io)
+    seq = read(stdin, String)
+    l1 = length(seq)
+
+    seq = replace(seq, r">.*\n|\n" => "")
+    l2 = length(seq)
+
+    repseq = seq
+    repseqfin = Threads.@spawn for (u, v) in subs
+        repseq = replace(repseq, u => v)
+    end
+
+    variant_counts = zeros(Int64, length(variants))
+    @inbounds Threads.@threads for i in 1:length(variants)
+        variant_counts[i] = length(collect(eachmatch(Regex(variants[i]), seq)))
+    end
+    for (v, k) in zip(variants, variant_counts)
+        write(io, v, ' ', string(k), '\n')
+    end#for
+
+    wait(repseqfin)
+    write(io, '\n', string(l1), '\n', string(l2), '\n', string(length(repseq)), '\n')
+end
+
+perf_regex_dna(stdout)
